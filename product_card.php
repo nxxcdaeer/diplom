61f3babb1c1539a5a625d500a0522becd78ce7d4
nxxcdaeer <163993089+nxxcdaeer@users.noreply.github.com>
2024-04-01 10:18:10 +0700

    начал карточку

---
+++ product_card.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная страница</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="all_body">
+
+        <!-- HEADER -->
+
+        <div class="main_header">
+            <div class="logo_main">
+                <img class="img_logo_main" src="images/logo.svg" alt="">
+            </div>
+        </div>
+
+        <!-- POD HEADER -->
+
+        <div class="pod_header_main">
+            <div class="pod_header_box_main">
+                <div class="boxes_main_flex"><a class="text_box_main" href="category.php">КАТЕГОРИИ</a></div>
+                <div class="boxes_main_flex"><a class="text_box_main" href="catalog.php">КАТАЛОГ</a></div>
+                <div class="boxes_main_flex"><a class="text_box_main" href="">КОРЗИНА</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- card -->
+
+
+    </div>
+</body>
+
+</html>
